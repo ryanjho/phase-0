74ffbceedf8e121ff0ca35227bba10ae576cce56
Ryan Ho <ryanjho90@gmail.com>
2015-10-24 06:16:26 -0400

    Add 4.3.2 files and reorganize 4.3 directory

---
+++ week-4/4.3/math/my_solution.rb
@@ -0,0 +1,28 @@
+# Math Methods
+
+# I worked on this challenge by myself
+
+# Your Solution Below
+
+def add(num_1, num_2)
+	return num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+	return num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+	return num_1 * num_2
+end
+
+def divide(num_1, num_2)
+	return num_1.to_f / num_2.to_f
+end
+
+#Test
+puts add(10,2)      # => 12
+puts subtract(10,2) # => 8
+puts multiply(10,2) # => 20
+puts divide(10,2)   # => 5.0 (*not* 5)
+
