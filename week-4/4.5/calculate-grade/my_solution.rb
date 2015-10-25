2d3381eeecca850cca857531c28c7705d654ed64
Ryan Ho <ryanjho90@gmail.com>
2015-10-25 11:39:27 -0400

    Add 4.5.2 challenge files

---
+++ week-4/4.5/calculate-grade/my_solution.rb
@@ -0,0 +1,22 @@
+# Calculate a Grade
+
+# I worked on this challenge by myself
+
+
+# Your Solution Below
+
+def get_grade(grade)
+	if grade >= 90
+		return "A"
+	elsif grade >= 80
+		return "B"
+	elsif grade >= 70
+		return "C"
+	elsif grade >= 65
+		return "D"
+	elsif grade >= 60
+		return "E"
+	else
+		return "F"
+	end
+end
