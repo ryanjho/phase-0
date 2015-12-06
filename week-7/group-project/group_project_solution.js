e1982b9120cc09037083cccdc8b75f2a258224fb
Ryan Ho <ryanjho90@gmail.com>
2015-12-06 17:14:03 +0800

    Add directory for 7.8 JavaScript Telephone challenge

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,18 @@
+// Person 1:  Tests to User Stories - Ryan Ho
+/*
+Overall objective: To create a basic statistics calculator that has several standard
+functions to caculate sum, average(mean) and median for a list of numbers.
+
+Story 1: As a user, I want to create a function called "sum" that will calculate the sum of a list of numbers
+Story 2: As a user, I want to create a function called "mean" that will calculate the average of a list of numbers
+Story 3: As a user, I want to create a function called "median" that will calculate median of a list of numbers
+*/
+
+
+// Person 2: 
+
+
+// Person 3:
+
+
+// Person 4:
